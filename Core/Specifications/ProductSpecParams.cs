4e5ac656a74eb238227661823546b4506cb8aedf
KovinePanda <mindaugas.keruckas@gmail.com>
2025-01-04 18:20:44 +0200

    End of section 14

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -7,7 +7,7 @@ public class ProductSpecParams
     private const int MaxPageSize = 50;
     public int PageIndex { get; set; } = 1;
 
-    private int _pageSize = 6;
+    private int _pageSize = 50;
     public int PageSize
     {
         get => _pageSize;
